25af4c4508f68e174bafee136d47f5875158b404
David Wen <dstwen@gmail.com>
2013-12-27 13:39:27 -0800

    display note beside item

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,12 @@
 class Item < ActiveRecord::Base
   has_and_belongs_to_many :lists
   has_many :notes
+
+  def note(list)
+    note = self.notes.select do |note|
+      note.item_id == self.id &&
+      note.list_id == list.id
+    end
+    return note[0]
+  end
 end
